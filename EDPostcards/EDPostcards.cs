6a2b5ea39e79a9b70c504f0fb8b748bcc9e48eb8
Alexis Lonchambon <alexis.lonchambon@sgs.com>
2019-03-18 17:10:57 +0100

    Initial commit

---
+++ EDPostcards/EDPostcards.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using EDPostcards.Services;
+using System.Threading.Tasks;
+
+namespace EDPostcards
+{
+    public class EDPostcards
+    {
+        private IAuthenticationContext _authenticationContext;
+        private IServiceProvider services;
+        internal static int logLvl = 3;
+
+        static void Main(string[] args) => RunAsync(args).GetAwaiter().GetResult();
+
+        public static async Task RunAsync(string[] args)
+        {
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+            var EDPostcards = new EDPostcards(args);
+            await EDPostcards.RunAsync();
+        }
+
+        public EDPostcards(string[] args)
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            services = serviceCollection.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// MAIN LOOP
+        /// </summary>
+        /// <returns></returns>
+        public async Task RunAsync()
+        {
+            Log.Message(Log.info, 
+                $"Booting up...\n" +
+                $"____________\n" +
+                $"{Assembly.GetExecutingAssembly().GetName().Name} " +
+                $"v{GetVersion()}\n" +
+                $"____________\n", "EDPostcard start");
+
+            // Set up your credentials (https://apps.twitter.com)
+            ITwitterCredentials creds = new TwitterCredentials("kpdduMJ1YIuXdanBYZddw8Z0f",
+                "Do1MRHot62rVIWK5YbkzlaG5ffS09IB0keV7Lvq99yvYDu5wDT",
+                "852893965858820101-5NlnPbgMqHxbjG4EOHHp01cVYgENIqW",
+                "TtjyVtDlIiqTYgwyCe3DiNU9zdBmZpRwSm7tCkCgT97Q8");
+            Auth.SetCredentials(creds);
+            Auth.ApplicationCredentials = creds;
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+
+            var authenticatedUser = User.GetAuthenticatedUser();
+            Log.Message(Log.info,
+                $"{authenticatedUser.Name} is connected :\n" +
+                $"  _____________\n" +
+                $"  ScreenName  : @{authenticatedUser.ScreenName}\n" +
+                $"  Desc        : {authenticatedUser.Description.Replace("\n", "")}\n" +
+                $"  Followers   : {authenticatedUser.FollowersCount}\n" +
+                $"  _____________",
+                "EDPostcard start");
+
+            var settings = authenticatedUser.GetAccountSettings();
+            // Publish the Tweet "Hello World" on your Timeline
+            //Tweet.PublishTweet("I'LL BE BACK");
+
+            var stream = Stream.CreateFilteredStream();
+
+            // Thread2
+            Thread t = new Thread(() =>
+            {
+                Thread.CurrentThread.Name = "StreamListener";
+                var user = User.GetAuthenticatedUser();
+                stream.AddTrack("EDPostcards");
+                stream.AddTrack("ED_Postcards");
+                stream.MatchingTweetReceived += (sender, args) =>
+                {
+                    var tweet = args.Tweet;
+                    if (!tweet.IsRetweet                            //  NOT A RETWEET
+                        && tweet.InReplyToStatusId == null          //  NOT A REPLY
+                        && tweet.CreatedBy.Id != user.Id            //  NOT SENT BY THE BOT
+                        && tweet.Media.Count > 0)                   //  MEDIAS INSIDE
+                    { Log.Message(Log.neutral, $"{tweet.CreatedBy.ScreenName}\n{tweet.Text}\n{tweet.Media.Count} media files", Thread.CurrentThread.Name); }
+                    Log.Message(Log.neutral, "A tweet containing 'ED Postcards' has been found; the tweet is '" + args.Tweet + "'", Thread.CurrentThread.Name);
+                };
+                stream.StartStreamMatchingAnyCondition();
+
+            });
+            t.Start();
+
+            t.Join();
+            await Task.Delay(-1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            Log.Message(Log.warning, "Shutting Down...", "EDPostcards Logout");
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Get services set up
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton(new TweetHandlerService());
+            serviceCollection.AddSingleton(new LoggingService());
+        }
+
+        /// <summary>
+        /// Get current version
+        /// </summary>
+        /// <returns></returns>
+        public static string GetVersion()
+        {
+            string rev = "b";
+#if DEBUG
+            rev = "a";
+#endif
+            return $"{Assembly.GetExecutingAssembly().GetName().Version.Major}.{Assembly.GetExecutingAssembly().GetName().Version.Minor}{rev}";
+        }
+    }
+}
---
+++ EDPostcards/Services/LoggingService.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EDPostcards.Services
+{
+    class LoggingService
+    {
+        private string _logDirectory { get; set; }
+        private string _logFile => Path.Combine(_logDirectory, $"{DateTime.UtcNow.ToString("yyyy-MM-dd")}.txt");
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LoggingService()
+        {
+            _logDirectory = Path.Combine(AppContext.BaseDirectory, "logs");
+        }
+    }
+
+
+    /// <summary>
+    /// Logging service to handle Application-side logs
+    /// </summary>
+    public class Log
+    {
+        public const int neutral = -1;
+        public const int critical = 0;
+        public const int error = 1;
+        public const int warning = 2;
+        public const int info = 3;
+        public const int verbose = 4;
+        public const int debug = 5;
+        /// <summary>
+        /// Log message function handling Discord Logging
+        /// </summary>
+        /// <param name="Severity">Message's severity</param>
+        /// <param name="message">Message's text</param>
+        /// <param name="source">Message's source</param>
+        public static void Message(int Severity, string message, string source = "")
+        {
+            switch (Severity)
+            {
+                case 0:
+                    Critical(message, source);
+                    break;
+                case 1:
+                    Error(message, source);
+                    break;
+                case 2:
+                    Warning(message, source);
+                    break;
+                case 3:
+                    Information(message, source);
+                    break;
+                case 4:
+                    Verbose(message, source);
+                    break;
+                case 5:
+                    Debug(message, source);
+                    break;
+                case -1:
+                    Neutral(message, source);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Logging debug (lvl 5)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        public static void Debug(string message, string source = "")
+        {
+            if (EDPostcards.logLvl >= 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                string Severity = "Debug".PadRight(8);
+                string[] lines = message.Split("\n");
+                foreach (var line in lines)
+                { Console.WriteLine($"[{Severity} {source.PadLeft(20)}][{DateTime.Now.ToString()}] : {line}"); }
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Logging Verbose (lvl 4)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        public static void Verbose(string message, string source = "")
+        {
+            if (EDPostcards.logLvl >= 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                string Severity = "Verbose".PadRight(8);
+                string[] lines = message.Split("\n");
+                foreach (var line in lines)
+                { Console.WriteLine($"[{Severity} {source.PadLeft(20)}][{DateTime.Now.ToString()}] : {line}"); }
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Logging Error (lvl 1)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        public static void Error(string message, string source = "")
+        {
+            if (EDPostcards.logLvl >= 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                string Severity = "Error".PadRight(8);
+                string[] lines = message.Split("\n");
+                foreach (var line in lines)
+                { Console.WriteLine($"[{Severity} {source.PadLeft(20)}][{DateTime.Now.ToString()}] : {line}"); }
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Logging Critical (lvl 0)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        public static void Critical(string message, string source = "")
+        {
+            if (EDPostcards.logLvl >= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                string Severity = "Critical".PadRight(8);
+                string[] lines = message.Split("\n");
+                foreach (var line in lines)
+                { Console.WriteLine($"[{Severity} {source.PadLeft(20)}][{DateTime.Now.ToString()}] : {line}"); }
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Logging Information (lvl 3) DEFAULT VALUE
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        public static void Information(string message, string source = "")
+        {
+            if (EDPostcards.logLvl >= 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                string Severity = "Info".PadRight(8);
+                string[] lines = message.Split("\n");
+                foreach (var line in lines)
+                { Console.WriteLine($"[{Severity} {source.PadLeft(20)}][{DateTime.Now.ToString()}] : {line}"); }
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Logging Warning (lvl 2)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        public static void Warning(string message, string source = "")
+        {
+            if (EDPostcards.logLvl >= 2)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                string Severity = "Warning".PadRight(8);
+                string[] lines = message.Split("\n");
+                foreach (var line in lines)
+                { Console.WriteLine($"[{Severity} {source.PadLeft(20)}][{DateTime.Now.ToString()}] : {line}"); }
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Logging Message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="source"></param>
+        public static void Neutral(string message, string source = "")
+        {
+            string Severity = "Normal".PadRight(8);
+            string[] lines = message.Split("\n");
+            foreach (var line in lines)
+            { Console.WriteLine($"[{Severity} {source.PadLeft(20)}][{DateTime.Now.ToString()}] : {line}"); }
+            Console.ResetColor();
+        }
+    }
+}
